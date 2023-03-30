5c71c95d367515150d9628915947114554030c7e
francesco44Hello <francescolongo44@gmail.com>
2023-03-30 12:47:13 +0100

    started NextJs app and linked employee/employer pages. Added logo as well.

---
+++ my-app/pages/employee.js
@@ -0,0 +1,14 @@
+import { Linden_Hill } from 'next/font/google'
+import Link from 'next/link'
+import React from 'react'
+
+const employeePage = () => {
+    return (
+        <div>
+            <h1>Employee page</h1>
+            <Link href='/'>Home</Link>
+        </div>
+    )
+}
+
+export default employeePage
---
+++ my-app/pages/employer.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const employerPage = () => {
+    return (
+        <div>
+            <h1>Employer</h1>
+        <Link href='/'></Link>
+        </div>
+    )
+}
+
+export default employerPage
