62048bdaff84518af7f99bb15da68daa778822bf
marthamaybennett <marthamaybennett123@gmail.com>
2023-03-30 15:14:45 +0100

    frontend adding new divs and classnames for form layout

--- my-app/pages/employee.js
+++ my-app/pages/employee.js
@@ -4,20 +4,82 @@ import React from "react";
 
 const employeePage = () => {
   return (
-    <div>
+    <>
       <div className="form">
         <form>
-          <input type="text" placeholder="First name"></input>
-          <input type="text" placeholder="First name"></input>
-          <input type="text" placeholder="First name"></input>
-          <input type="text" placeholder="First name"></input>
-          <input type="text" placeholder="First name"></input>
-          <input type="text" placeholder="First name"></input>
-          <input type="checkbox"></input>
+          <div className="input-label"> 
+            <label for="name">Name:</label>
+            <br></br>
+            <input
+              className="input"
+              type="text"
+              placeholder="Name"
+              name="Name"
+            ></input>
+          </div>
+          <div className="input-label">
+            <label for="Start date">Start date:</label>
+            <br></br>
+            <input
+              className="input"
+              type="date"
+              placeholder="Start date"
+              name="Start date"
+            ></input>
+          </div>
+          <div className="input-label">
+            <label for="End date">End date:</label>
+            <input
+              className="input"
+              type="date"
+              placeholder="End date"
+              name="End date"
+            ></input>
+          </div>
+          <label for="Skills:">Skills:</label>
+          <div className="check-boxes">
+            <input
+              type="checkbox"
+              id="Javascript"
+              name="Javascript"
+              defaultValue="Javascript"
+            />
+            <label htmlFor="Javascript"> Javascript</label>
+            <br />
+            <input
+              type="checkbox"
+              id="Python"
+              name="Python"
+              defaultValue="Python"
+            />
+            <label htmlFor="Python"> Python</label>
+            <br />
+            <input type="checkbox" id="Java" name="Java" defaultValue="Java" />
+            <label htmlFor="Java"> Java</label>
+            <br />
+            <input type="checkbox" id="AWS" name="AWS" defaultValue="AWS" />
+            <label htmlFor="AWS"> AWS</label>
+            <br />
+            <input type="checkbox" id="CSS" name="CSS" defaultValue="CSS" />
+            <label htmlFor="CSS"> CSS</label>
+            <br />
+          </div>
+          <button className="submit">Submit</button>
         </form>
+        
+        <Link
+          href="/"
+          style={{
+            textDecoration: "none",
+            border: "1px solid black",
+            padding: "5px",
+            color: "black",
+          }}
+        >
+          Home
+        </Link>
       </div>
-      <Link href="/">Home</Link>
-    </div>
+    </>
   );
 };
 
